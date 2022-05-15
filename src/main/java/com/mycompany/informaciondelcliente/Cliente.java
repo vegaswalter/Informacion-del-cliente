8b9ab8833866771530e271b3f8a501c5182ba299
vegaswalter <damianvegasrodriguez@gmail.com>
2022-05-15 10:53:24 -0300

    Informacion del Cliente

---
+++ src/main/java/com/mycompany/informaciondelcliente/Cliente.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.informaciondelcliente;
+
+/**
+ *
+ * @author walte
+ */
+public class Cliente {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+   
+    public String getNombreCompleto(){
+        return nombre + " " + apellido;
+    }
+}
+ 
